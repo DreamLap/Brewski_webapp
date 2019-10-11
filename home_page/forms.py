00fb603b305c9717acd3a8a7d11e51bf29bf478b
Andrew H <drewww97@aol.com>
2019-10-11 17:35:05 -0500

    Fixed url pathing in html files

--- home_page/forms.py
+++ home_page/forms.py
@@ -1,4 +1,11 @@
 from django import forms
 
+'''
 class JournalForm(forms.Form):
     journal_name = forms.CharField(label='JournalName', max_length=100)
+'''
+	
+class JournalForm(forms.Form):
+	hop_name = forms.CharField(max_length=100)
+	hop_alpha = forms.FloatField(min_value = 0.0, max_value = 100.0)
+	hop_amount = forms.FloatField()
